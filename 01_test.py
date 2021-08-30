7c033b28614475212db96238184cb03422b22da1
Idris Raja <idris.raja@gmail.com>
2021-08-30 16:35:24 -0500

    left, right

--- 01_test.py
+++ 01_test.py
@@ -1,24 +1,24 @@
 from hypothesis.strategies import *
 from hypothesis import given
 
-def NOT(x):   return x(FALSE)(TRUE)
-def TRUE(x):  return lambda y: x
-def FALSE(x): return lambda y: y
-def AND(x):   return lambda y: x(y)(x)
-def OR(x):    return lambda y: x(x)(y)
-def left(a):  return lambda b: a
-def right(a): return lambda b: b
-ONE   = lambda f: lambda x: f(x)
-TWO   = lambda f: lambda x: f(f(x))
-THREE = lambda f: lambda x: f(f(f(x)))
-FOUR  = lambda f: lambda x: f(f(f(f(x))))
-ZERO  = lambda f: lambda x: x
-SUCC  = lambda n: lambda f: lambda x: f(n(f)(x))
-ADD = lambda x: lambda y: y(SUCC)(x)
-MUL = lambda x: lambda y: lambda f: y(x(f))
+def left(x): return lambda y: x
+def right(x): return lambda y: y
 
-@given(integers(), floats(allow_nan=False))
+@given(integers(), integers())
 def test_left(a, b): assert left(a)(b) == a
 
-@given(integers(), floats(allow_nan=False))
+@given(integers(), integers())
 def test_right(a, b): assert right(a)(b) == b
+
+
+#  left
+#  right
+#  NOT
+#  TRUE
+#  FALSE
+#  AND
+#  OR
+#  0-4
+#  succ
+#  add
+#  mul
